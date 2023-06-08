52f2c65d1f99d5c60819cd0c3402957d148a1ed7
Dmitriy <gdmit365@yahoo.com>
2023-06-09 01:28:06 +0400

    change Cang.java

--- app/src/main/java/hexlet/code/Cong.java
+++ app/src/main/java/hexlet/code/Cong.java
@@ -3,7 +3,7 @@ package hexlet.code;
 public class Cong {
     public static void congratulation(String name, byte numberGame, boolean exam) {
         if ((numberGame == 3) & (exam)) {
-            System.out.println("Congratulations, " + name);
+            System.out.println("Congratulations, " + name + "!");
         }
     }
 }
